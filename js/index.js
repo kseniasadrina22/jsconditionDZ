000292eb3d7cc552818e08e7ccdb6ddf3a3ec370
kseniasadrina22 <kseniasadrina212@gmail.com>
2022-11-08 16:00:41 +0200

    Add Part2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 //Таски на умови.
 
 //1) Запитати змінну у користувача. Якщо змінна дорівнює числу 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
-//let userValue = Number(prompt("Enter a number:"));
-//const inputValue = 10;
+let userValue = Number(prompt("Enter a number:"));
+const inputValue = 10;
 
-//if (userValue === inputValue) {
-//  console.log("Number" + userValue + "is equal to 10 ");
-//} else {
-//  console.log("The number" + userValue + "is not equal 10 ");
-//}
+if (userValue === inputValue) {
+  console.log("Number" + userValue + "is equal to 10 ");
+} else {
+  console.log("The number" + userValue + "is not equal 10 ");
+}
 
 //2) Напишіть дві перевірки.
 //Якщо змінна test дорівнює true, виведіть 'Вірно', інакше виведіть 'Невірно'.
@@ -43,7 +43,39 @@ if (purchase > 500) {
 //Кожне завдання вирішити циклами while, for.
 
 //1) Виведення чисел від 25 до 0 (порядок зменшення).
+let i = 25;
+while ((i < 26, i > -1)) {
+  console.log(i);
+  i--;
+}
+
+for (i = 25; i >= 0; i--) {
+  console.log(i);
+}
 
 //2) Виведення чисел від 10 до 50, які кратні 5.
+let f = 10;
+while (f <= 50) {
+  console.log(f);
+  f += 5;
+}
+
+for (f = 10; f <= 50; f += 5) {
+  console.log(f);
+}
 
 //3) Знайти суму чисел в межах від 1 до 100.
+
+let n1 = 0;
+let n2 = 0;
+
+while (n1++ < 100) {
+  console.log(n2);
+}
+
+let m1 = 0;
+let m2 = 0;
+for (let m1 = 0; m1 <= 100; m1++) {
+  m2 += m1;
+}
+console.log(m2);
